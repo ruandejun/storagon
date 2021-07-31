9360797cb508f294463dcd23fb2cb54395325b1f
tuan nguyen <ruandejun@gmail.com>
2021-07-31 22:25:10 +0800

    fix urlencode

--- storagon/decorator.py
+++ storagon/decorator.py
@@ -65,6 +65,7 @@ def signature_test():
 			correct_signature = hashlib.md5(settings.SECRET_KEY + params).hexdigest()
 			if correct_signature != signature:
 				# print params,'\n',params2
+				print(params2)
 				correct_signature2 = hashlib.md5(settings.SECRET_KEY + params2.encode('utf-8')).hexdigest()
 				if correct_signature2 != signature:
 					return HttpResponseForbidden(u"correct_signature=%s or %s, but signature=%s" % (correct_signature, correct_signature2, signature))

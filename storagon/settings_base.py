0f6f86f3f686097fff0324c69230767c194983d9
tuan nguyen <ruandejun@gmail.com>
2021-07-31 18:45:04 +0800

    update template

--- storagon/settings_base.py
+++ storagon/settings_base.py
@@ -40,6 +40,16 @@ TEMPLATES = [
                  ],
         'APP_DIRS': True,
         'OPTIONS': {
+			'context_processors': [
+				'django.contrib.auth.context_processors.auth',
+				'django.template.context_processors.request',
+				'django.template.context_processors.debug',
+				'django.template.context_processors.i18n',
+				'django.template.context_processors.media',
+				'django.template.context_processors.static',
+				'django.template.context_processors.tz',
+				'django.contrib.messages.context_processors.messages',
+			],
         },
     },
 ]
@@ -76,19 +86,15 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-	'corsheaders.middleware.CorsMiddleware',	# CORS must put before CommonMiddleware
-
-	'system_configure.controllers.Tool.DisableCSRF',
-
-	
+	'django.contrib.auth.middleware.AuthenticationMiddleware'
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
-# 'django.middleware.csrf.CsrfViewMiddleware', #uncomment to enable csrf check
-	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'corsheaders.middleware.CorsMiddleware',  # CORS must put before CommonMiddleware
+	'system_configure.controllers.Tool.DisableCSRF',
 )
+
 
 ROOT_URLCONF = 'storagon.urls'
 

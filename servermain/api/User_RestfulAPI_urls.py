dc94884bfc65876289e94f8ef48788d68110c853
tuan nguyen <ruandejun@gmail.com>
2021-07-31 15:59:01 +0800

    update url clientapi

--- servermain/api/User_RestfulAPI_urls.py
+++ servermain/api/User_RestfulAPI_urls.py
@@ -30,12 +30,12 @@ from .User_RestfulAPI import CurrentUserProfileView,CurrentUserAccountBalanceVie
 from system_configure.controllers.Tool import FullRouter
 
 router = FullRouter('user');
-router.register(r'profile', CurrentUserProfileView, base_name='Profile')
-router.register(r'accountBalance', CurrentUserAccountBalanceView, base_name='AccountBalance')
-router.register(r'websiteAgency', CurrentUserWebsiteAgencyView, base_name='WebsiteAgency')
-router.register(r'userApplyList', CurrentUserUserApplyList, base_name='UserApplyList')
-router.register(r'userApplyView', CurrentUserUserApplyView, base_name='UserApplyView')
-router.register(r'auth', CurrentUserView, base_name='UserView')
+router.register(r'profile', CurrentUserProfileView, basename='Profile')
+router.register(r'accountBalance', CurrentUserAccountBalanceView, basename='AccountBalance')
+router.register(r'websiteAgency', CurrentUserWebsiteAgencyView, basename='WebsiteAgency')
+router.register(r'userApplyList', CurrentUserUserApplyList, basename='UserApplyList')
+router.register(r'userApplyView', CurrentUserUserApplyView, basename='UserApplyView')
+router.register(r'auth', CurrentUserView, basename='UserView')
 
 urlpatterns = router.urls
 

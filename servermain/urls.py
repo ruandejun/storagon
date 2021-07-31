9a600d3e2fc2a1091a29aa983e50b6e87e06771b
tuan nguyen <ruandejun@gmail.com>
2021-07-31 15:54:18 +0800

    update url clientapi

--- servermain/urls.py
+++ servermain/urls.py
@@ -1,21 +1,21 @@
 from django.conf import settings;
 from django.conf.urls import include, url
 from django.views.generic import RedirectView, TemplateView
-# from . import api
-from .api import Session_ClientAPI_urls, User_ClientAPI_urls, File_ClientAPI_urls, UserStatistics_ClientAPI_urls, Premium_ClientAPI_urls, File_PrivateAPI_urls, Session_PrivateAPI_urls
-# from .api import User_RestfulAPI_urls, File_RestfulAPI_urls
-import system_configure.urls
+
+# from .api import Session_ClientAPI_urls, User_ClientAPI_urls, File_ClientAPI_urls, UserStatistics_ClientAPI_urls, Premium_ClientAPI_urls, File_PrivateAPI_urls, Session_PrivateAPI_urls
+# # from .api import User_RestfulAPI_urls, File_RestfulAPI_urls
+# import system_configure.urls
 from . import restful_urls, storagon_urls, junshare_urls
 from .payment_views import *
 
 urlpatterns = [
-	url(r'^clapi/session/', Session_ClientAPI_urls),
-	url(r'^clapi/user/', User_ClientAPI_urls),
-	url(r'^clapi/file/', File_ClientAPI_urls),
-	url(r'^clapi/userstats/', UserStatistics_ClientAPI_urls),
-	url(r'^clapi/premium/', Premium_ClientAPI_urls),
-	url(r'^prapi/file/', File_PrivateAPI_urls),
-	url(r'^prapi/session/', Session_PrivateAPI_urls),
+	url(r'^clapi/session/', include('servermain.api.Session_ClientAPI_urls', namespace='Session_ClientAPI')),
+	url(r'^clapi/user/', include('servermain.api.User_ClientAPI_urls', namespace='User_ClientAPI')),
+	url(r'^clapi/file/', include('servermain.api.File_ClientAPI_urls', namespace='File_ClientAPI')),
+	url(r'^clapi/userstats/', include('servermain.api.UserStatistics_ClientAPI_urls', namespace='UserStatistics_ClientAPI')),
+	url(r'^clapi/premium/', include('servermain.api.Premium_ClientAPI_urls', namespace='Premium_ClientAPI')),
+	url(r'^prapi/file/', include('servermain.api.File_PrivateAPI_urls', namespace='File_PrivateAPI')),
+	url(r'^prapi/session/', include('servermain.api.Session_PrivateAPI_urls', namespace='Session_PrivateAPI')),
 
 	# url(r'^restful/user/', include(User_RestfulAPI_urls, namespace='User_RestfulAPI')),
 	# url(r'^restful/file/', include(File_RestfulAPI_urls, namespace='File_RestfulAPI')),
@@ -24,7 +24,7 @@ urlpatterns = [
 	url(r'^buypremium/(\d+)/(\d+)/', buyPremium, name='buyPremium'),
 	url(r'^paygatecallback/(\w+)/', paygateCallBack, name='paygateCallBack'),
 
-	url(r'^sys/', include(system_configure.urls)),
+	url(r'^sys/', include('system_configure.urls')),
 
 
 ]

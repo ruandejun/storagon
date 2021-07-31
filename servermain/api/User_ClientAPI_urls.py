24055fa90d3e0a5e5fb4e2ad259195ee79e43a25
tuan nguyen <ruandejun@gmail.com>
2021-07-31 17:35:17 +0800

    fix mongoviewset

--- servermain/api/User_ClientAPI_urls.py
+++ servermain/api/User_ClientAPI_urls.py
@@ -12,24 +12,24 @@ from django.conf.urls import url
 from django.views.generic import RedirectView, TemplateView
 
 # from .User_ClientAPI import *
-#
-# urlpatterns = [
-# 	#url(r'^home/$', TemplateView.as_view(template_name='example/home.html'), name="home"),
-# 	#url(r'^/?$', RedirectView.as_view(pattern_name="home")),
-#
-# 	url(r'^login/', custom_login, name='custom_login'),
-# 	url(r'^logout/', custom_logout, name='custom_logout'),
-# 	url(r'^getUserInfo/', getUserInfo, name='getUserInfo'),
-# 	url(r'^updateUserInfo/', updateUserInfo, name='updateUserInfo'),
-# 	url(r'^getUserBalance/', getUserBalance, name='getUserBalance'),
-# 	url(r'^signup/', signup, name='signup'),
-# 	url(r'^sendResetPasswordEmail/', sendResetPasswordEmail, name='sendResetPasswordEmail'),
-# 	# url(r'^createTemporaryUser/', 'createTemporaryUser', name='createTemporaryUser'),
-# 	# url(r'^signupTemporaryUserAccount/', 'signupTemporaryUserAccount', name='signupTemporaryUserAccount'),
-# 	url(r'^resendActivationEmail/', resendActivationEmail, name='resendActivationEmail'),
-# 	url(r'^applyToBecomeAffiliate/', applyToBecomeAffiliate, name='applyToBecomeAffiliate'),
-# 	url(r'^applyToChangeAffiliateMode/', applyToChangeAffiliateMode, name='applyToChangeAffiliateMode'),
-# 	url(r'^addWebsiteAgencyDomain/', addWebsiteAgencyDomain, name='addWebsiteAgencyDomain'),
-# 	url(r'^getListWebsiteAgency/', getListWebsiteAgency, name='getListWebsiteAgency'),
-#
-# ]
+
+urlpatterns = [
+	#url(r'^home/$', TemplateView.as_view(template_name='example/home.html'), name="home"),
+	#url(r'^/?$', RedirectView.as_view(pattern_name="home")),
+
+	# url(r'^login/', custom_login, name='custom_login'),
+	# url(r'^logout/', custom_logout, name='custom_logout'),
+	# url(r'^getUserInfo/', getUserInfo, name='getUserInfo'),
+	# url(r'^updateUserInfo/', updateUserInfo, name='updateUserInfo'),
+	# url(r'^getUserBalance/', getUserBalance, name='getUserBalance'),
+	# url(r'^signup/', signup, name='signup'),
+	# url(r'^sendResetPasswordEmail/', sendResetPasswordEmail, name='sendResetPasswordEmail'),
+	# # url(r'^createTemporaryUser/', 'createTemporaryUser', name='createTemporaryUser'),
+	# # url(r'^signupTemporaryUserAccount/', 'signupTemporaryUserAccount', name='signupTemporaryUserAccount'),
+	# url(r'^resendActivationEmail/', resendActivationEmail, name='resendActivationEmail'),
+	# url(r'^applyToBecomeAffiliate/', applyToBecomeAffiliate, name='applyToBecomeAffiliate'),
+	# url(r'^applyToChangeAffiliateMode/', applyToChangeAffiliateMode, name='applyToChangeAffiliateMode'),
+	# url(r'^addWebsiteAgencyDomain/', addWebsiteAgencyDomain, name='addWebsiteAgencyDomain'),
+	# url(r'^getListWebsiteAgency/', getListWebsiteAgency, name='getListWebsiteAgency'),
+
+]

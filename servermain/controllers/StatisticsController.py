2fefd1c9754f0b9762e9fad033d00cecd1e4d963
tuan nguyen <ruandejun@gmail.com>
2021-08-01 20:27:55 +0800

    update USERstorage

--- servermain/controllers/StatisticsController.py
+++ servermain/controllers/StatisticsController.py
@@ -40,8 +40,8 @@ def countSessionByDayOfFileOwnerUser(user_id, session_type,session_status,from_d
 			{ '$sort' : {"_id": 1}},
 		]
 	);
-	print('countSessionResult',countSessionResult)
-	return list(countSessionResult)['result'];
+	print('countSessionResult===',countSessionResult)
+	return countSessionResult['result'];
 
 
 def countAndSumTransactionByDayOfUser(user_id, transaction_type, from_date,to_date):

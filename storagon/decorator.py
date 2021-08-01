f4fcdc42303be7f6616ce80a61bb38e562a1f21d
tuan nguyen <ruandejun@gmail.com>
2021-08-01 16:57:01 +0800

    update sort

--- storagon/decorator.py
+++ storagon/decorator.py
@@ -31,70 +31,71 @@ from storagon.enum import *
 
 
 def signature_test():
-	"""
-	Decorator for views that checks that the user passes the given test,
-	redirecting to the log-in page if necessary. The test should be a callable
-	that takes the user object and returns True if the user passes.
-	"""
+    """
+    Decorator for views that checks that the user passes the given test,
+    redirecting to the log-in page if necessary. The test should be a callable
+    that takes the user object and returns True if the user passes.
+    """
 
-	def decorator(view_func):
-		# @wraps(view_func, assigned=available_attrs(view_func))
-		def _wrapped_view(request, *args, **kwargs):
-			# request=HttpRequest();
-			signature = request.META.get('HTTP_SIGNATURE_AUTHORIZATION')
-			if not signature:
-				# print request.META;
-				return HttpResponseForbidden(u"Signature is required!")
+    def decorator(view_func):
+        # @wraps(view_func, assigned=available_attrs(view_func))
+        def _wrapped_view(request, *args, **kwargs):
+            # request=HttpRequest();
+            signature = request.META.get('HTTP_SIGNATURE_AUTHORIZATION')
+            if not signature:
+                # print request.META;
+                return HttpResponseForbidden(u"Signature is required!")
 
-			if request.method == 'GET':
-				params = request.get_full_path()
-				prefix='http://'
-				if request.is_secure(): prefix='https://'
-				params2 = prefix + request.get_host() + request.get_full_path(); #request.build_absolute_uri(params)
+            if request.method == 'GET':
+                params = request.get_full_path()
+                prefix='http://'
+                if request.is_secure(): prefix='https://'
+                params2 = prefix + request.get_host() + request.get_full_path(); #request.build_absolute_uri(params)
 
-			elif request.method == 'POST':
-				params2 = request.body  # case 2
-				dataItems = request.POST.items()
-				# dataItems
-				#convert unicode to fix urlencode error
-				params = urlencode([(k.encode('utf-8'), v.encode('utf-8')) for k, v in dataItems]);
-				# params = urllib.urlencode(dataItems)  # case 1
-			else:
-				return HttpResponseForbidden(u"Invalid Method")
+            elif request.method == 'POST':
+                params2 = request.body  # case 2
+                dataItems = request.POST.items()
+                dataItems = sorted(dataItems)
+                # dataItems.sort()
+                #convert unicode to fix urlencode error
+                params = urlencode([(k, v) for k, v in dataItems]);
+                # params = urllib.urlencode(dataItems)  # case 1
+            else:
+                return HttpResponseForbidden(u"Invalid Method")
 
-			correct_signature = hashlib.md5(settings.SECRET_KEY.encode('utf-8') + params.encode('utf-8')).hexdigest()
-			if correct_signature != signature:
-				# print params,'\n',params2
-				correct_signature2 = hashlib.md5(settings.SECRET_KEY.encode('utf-8') + params2.encode('utf-8')).hexdigest()
-				if correct_signature2 != signature:
-					return HttpResponseForbidden(u"correct_signature=%s or %s, but signature=%s" % (correct_signature, correct_signature2, signature))
-					# return HttpResponseForbidden("Invalid Signature");
+            correct_signature = hashlib.md5(settings.SECRET_KEY.encode('utf-8') + params.encode('utf-8')).hexdigest()
+            if correct_signature != signature:
+                # print params,'\n',params2
+                correct_signature2 = hashlib.md5(settings.SECRET_KEY.encode('utf-8') + params2.encode('utf-8')).hexdigest()
+                if correct_signature2 != signature:
+                    return HttpResponseForbidden(u"correct_signature=%s or %s, but signature=%s" % (correct_signature, correct_signature2, signature))
+                    # return HttpResponseForbidden("Invalid Signature");
 
-			return view_func(request, *args, **kwargs)
-		return _wrapped_view
-	return decorator
+            return view_func(request, *args, **kwargs)
+        return _wrapped_view
+    return decorator
 
 
 def login_required_ajax():
-	"""
-	Just make sure the user is authenticated to access a certain ajax view
+    """
+    Just make sure the user is authenticated to access a certain ajax view
 
-	Otherwise return a HttpResponse 401 - authentication required
-	instead of the 302 redirect of the original Django decorator
-	"""
-	def decorator(view_func):
-		def _wrapped_view(request, *args, **kwargs):
-			if request.user and request.user.is_authenticated:
-				return view_func(request, *args, **kwargs)
-			else:
-				return HttpResponse(status=401)
-		return _wrapped_view
-	return decorator
+    Otherwise return a HttpResponse 401 - authentication required
+    instead of the 302 redirect of the original Django decorator
+    """
+    def decorator(view_func):
+        def _wrapped_view(request, *args, **kwargs):
+            if request.user and request.user.is_authenticated:
+                return view_func(request, *args, **kwargs)
+            else:
+                return HttpResponse(status=401)
+        return _wrapped_view
+    return decorator
 
 
 def banned_check(user):
-	if not user or not user.is_authenticated:
-		return False  # guest users are not allowed
-	if user.profile.account_status == AccountStatus.banned:
-		return False
-	return True
+    if not user or not user.is_authenticated:
+        return False  # guest users are not allowed
+    if user.profile.account_status == AccountStatus.banned:
+        return False
+    return True

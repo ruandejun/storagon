e8a65abe8d9e58ff237e51f749e5ba95f1e43b1c
tungmt <tungmt@outlook.com>
2021-09-21 01:59:10 +0700

    update header

--- storagon/settings_base.py
+++ storagon/settings_base.py
@@ -325,7 +325,6 @@ ADMIN_RESUMABLE_SUBDIR = 'realfile/'
 ADMIN_RESUMABLE_SHOW_THUMB = True
 
 
-CORS_ORIGIN_ALLOW_ALL = True	# allow all domain
 CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ORIGIN_WHITELIST = (
 #	 'storagon.com',

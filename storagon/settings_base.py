aec2c1f2902b1983857ca292321bbeef35438249
tungmt <tungmt@outlook.com>
2021-09-21 01:56:27 +0700

    update setting

--- storagon/settings_base.py
+++ storagon/settings_base.py
@@ -326,6 +326,7 @@ ADMIN_RESUMABLE_SHOW_THUMB = True
 
 
 CORS_ORIGIN_ALLOW_ALL = True	# allow all domain
+CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ORIGIN_WHITELIST = (
 #	 'storagon.com',
 #	 'test.storagon.com',
@@ -441,7 +442,7 @@ FILE_MANAGER_ROOT_FOLDER = 'download' #must be inside MEDIA_ROOT dir
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # serverfile Settings
-SERVER_MAIN_URL = 'https://api.storagon.com'
+SERVER_MAIN_URL = 'https://storagon.com'
 SERVER_FILE_ID = 1
 # ROOT_URLCONF = 'storagon.urls_serverFile'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

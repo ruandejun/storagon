f1e5960094c67e9d1ff9723b6c54811bf4b4bb0c
tuan nguyen <ruandejun@gmail.com>
2021-11-30 15:50:37 +1000

    add celery

--- storagon/settings.py
+++ storagon/settings.py
@@ -8,7 +8,8 @@ ENABLE_ENCRYPTION = False
 # ENABLE_ENCRYPTION = True
 
 TIME_ZONE = 'Asia/Bangkok'
-CELERY_TIMEZONE = 'Asia/Bangkok'
+
+
 # LANGUAGE_CODE = 'vi'
 
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
@@ -17,44 +18,6 @@ REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
 )
 
 
-# Caching
-CACHES = {  # config for docker container memcached
-    'default': {  # Cluster MemCache (recommend for django-cache-machine)
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': ['memcached:11211'],
-        'PREFIX': 'storagon_',
-        'TIMEOUT': None,  # Keep cache forever, or for seconds
-    }
-}
-
-REDISDB = {
-	'DB': 0,
-	'HOST': 'redis',
-	'PORT': 6379,
-	'PASSWORD': 'hanoi123'
-}
-# CELERY settings with password AUTH
-
-CELERY_RESULT_BACKEND = "redis://default:hanoi123@redis:6379/0"
-CELERY_REDIS_HOST = "redis"
-CELERY_REDIS_PORT = 6379
-CELERY_REDIS_PASSWORD = 'hanoi123'
-CELERY_REDIS_DB = 0
-# CELERY_TIMEZONE = TIME_ZONE
-# CELERY_ENABLE_UTC = False
-CELERY_BROKER_URL = 'redis://default:hanoi123@redis:6379/0'
-CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
-CELERY_BROKER_CONNECTION_TIMEOUT = 10
-CELERY_ALWAYS_EAGER = False
-CELERY_ACKS_LATE = True
-CELERY_TASK_PUBLISH_RETRY = True
-CELERY_DISABLE_RATE_LIMITS = False
-CELERY_BROKER_TRANSPORT_OPTIONS = {
-    'max_retries': 4,
-    'interval_start': 0,
-    'interval_step': 0.5,
-    'interval_max': 3,
-}
 
 # BROKER_URL = 'redis://rediscache:6379/0'	# using resdis
 # CELERY_RESULT_BACKEND = 'redis://rediscache:6379/0'	# using resdis
--- storagon/settings_base.py
+++ storagon/settings_base.py
@@ -107,16 +107,50 @@ DATABASES = {
 		'PORT': 5432,  # Set to empty string for default. Not used with sqlite3.
 	}
 }
+
 MONGODB = {
 	'NAME': 'storagon',
 	'HOST': 'mongodb',
 	'PORT': 27017,
 }
+
+# Caching
+CACHES = {  # config for docker container memcached
+    'default': {  # Cluster MemCache (recommend for django-cache-machine)
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': ['memcached:11211'],
+        'PREFIX': 'storagon_',
+        'TIMEOUT': None,  # Keep cache forever, or for seconds
+    }
+}
+
 REDISDB = {
 	'DB': 0,
-	'HOST': 'rediscache',
+	'HOST': 'redis',
 	'PORT': 6379,
-	'PASSWORD': '',
+	'PASSWORD': 'hanoi123'
+}
+# CELERY settings with password AUTH
+CELERY_TIMEZONE = 'Asia/Bangkok'
+CELERY_RESULT_BACKEND = "redis://default:hanoi123@redis:6379/0"
+CELERY_REDIS_HOST = "redis"
+CELERY_REDIS_PORT = 6379
+CELERY_REDIS_PASSWORD = 'hanoi123'
+CELERY_REDIS_DB = 0
+# CELERY_TIMEZONE = TIME_ZONE
+# CELERY_ENABLE_UTC = False
+CELERY_BROKER_URL = 'redis://default:hanoi123@redis:6379/0'
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+CELERY_BROKER_CONNECTION_TIMEOUT = 10
+CELERY_ALWAYS_EAGER = False
+CELERY_ACKS_LATE = True
+CELERY_TASK_PUBLISH_RETRY = True
+CELERY_DISABLE_RATE_LIMITS = False
+CELERY_BROKER_TRANSPORT_OPTIONS = {
+    'max_retries': 4,
+    'interval_start': 0,
+    'interval_step': 0.5,
+    'interval_max': 3,
 }
 
 IS_RUNNING_UNIT_TEST = False

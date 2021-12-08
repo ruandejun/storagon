d5f33b5a79a32d07a190d9f12cec3c22d76d7021
tuan nguyen <ruandejun@gmail.com>
2021-12-08 10:54:39 +1000

    upate networks

--- storagon/settings_base.py
+++ storagon/settings_base.py
@@ -121,7 +121,7 @@ MONGODB = {
 
 import mongoengine
 
-db_connection = mongoengine.connect(db=MONGODB['NAME'], host=MONGODB['HOST'])
+db_connection = mongoengine.connect(db=MONGODB['NAME'], host=MONGODB['HOST'], username=MONGODB['USER'], password=MONGODB['PASSWORD'])
 if IS_RUNNING_UNIT_TEST:
 	db_connection.drop_database(MONGODB['NAME'])
 	print("Clear MONGODB on launch")

61c41423fc4a9e3aecc8a76b21d97f4e1c3ebcbe
ruandejun <ruandejun@gmail.com>
2022-09-04 10:45:50 +1000

    update telegram get browser profiles

--- storagon/middleware.py
+++ storagon/middleware.py
@@ -1,13 +1,21 @@
 from storagon.utils import set_current_user, remove_current_user
-from rest_framework.decorators import api_view
-@api_view(['GET', 'POST', 'PUT'])
+from re import sub
+from rest_framework.authtoken.models import Token
+
 class CurrentUserMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         # One-time configuration and initialization.
 
     def __call__(self, request):
-
+        header_token = request.META.get('HTTP_AUTHORIZATION', None)
+        if header_token is not None:
+            try:
+                token = sub('Token ', '', request.META.get('HTTP_AUTHORIZATION', None))
+                token_obj = Token.objects.get(key=token)
+                request.user = token_obj.user
+            except Token.DoesNotExist:
+                pass
         set_current_user(getattr(request, 'user', None))
 
         response = self.get_response(request)

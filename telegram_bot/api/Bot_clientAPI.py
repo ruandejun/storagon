44c9b3972f43cfc5c5fc83c7d2ccdd53750d8f28
ruandejun <ruandejun@gmail.com>
2022-10-05 11:24:40 +1000

    update webrtc

--- telegram_bot/api/Bot_clientAPI.py
+++ telegram_bot/api/Bot_clientAPI.py
@@ -812,31 +812,18 @@ def get_inject_info(request):
     })();
     '''  
     inject_data['webrtc'] = '''
-        var script = document.getElementById("webrtc-control");
-        var head = document.documentElement || document.head || document.querySelector("head");
-        if (!script) {
-            script = document.createElement('script');
-            script.type = "text/javascript";
-            script.setAttribute("id", "webrtc-control");
-            if (head) head.appendChild(script);
-        }
-        /*  */
-
-        try {
-            var webrtc = '(' + function() {
-                if (typeof navigator.getUserMedia !== "undefined") navigator.getUserMedia = undefined;
-                if (typeof window.MediaStreamTrack !== "undefined") window.MediaStreamTrack = undefined;
-                if (typeof window.RTCPeerConnection !== "undefined") window.RTCPeerConnection = undefined;
-                if (typeof navigator.webkitGetUserMedia !== "undefined") navigator.webkitGetUserMedia = undefined;
-                if (typeof window.RTCSessionDescription !== "undefined") window.RTCSessionDescription = undefined;
-                if (typeof window.webkitMediaStreamTrack !== "undefined") window.webkitMediaStreamTrack = undefined;
-                if (typeof window.webkitRTCPeerConnection !== "undefined") window.webkitRTCPeerConnection = undefined;
-                if (typeof window.webkitRTCSessionDescription !== "undefined") window.webkitRTCSessionDescription = undefined;
-            } + ')();';
-            /*  */
-            script.textContent = webrtc;
-        } catch (e) {}
-      '''
+      (function disableWebrtc() {
+        if (typeof navigator.getUserMedia !== "undefined") navigator.getUserMedia = undefined;
+        if (typeof window.MediaStreamTrack !== "undefined") window.MediaStreamTrack = undefined;
+        if (typeof window.RTCPeerConnection !== "undefined") window.RTCPeerConnection = undefined;
+        if (typeof navigator.webkitGetUserMedia !== "undefined") navigator.webkitGetUserMedia = undefined;
+        if (typeof window.RTCSessionDescription !== "undefined") window.RTCSessionDescription = undefined;
+        if (typeof window.webkitMediaStreamTrack !== "undefined") window.webkitMediaStreamTrack = undefined;
+        if (typeof window.webkitRTCPeerConnection !== "undefined") window.webkitRTCPeerConnection = undefined;
+        if (typeof window.webkitRTCSessionDescription !== "undefined") window.webkitRTCSessionDescription = undefined;
+      })();
+      console.log('==disableWebrtc=='); 
+    '''
           
     
     

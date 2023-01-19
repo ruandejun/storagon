e23c51ea9a4db80c092ce6bf0b2c8aac7f5fbf5c
ruandejun <ruandejun@gmail.com>
2023-01-19 12:36:36 +1000

    update storagon

--- telegram_bot/api/Bot_clientAPI.py
+++ telegram_bot/api/Bot_clientAPI.py
@@ -2074,8 +2074,8 @@ def get_checker_task(request):
     if list_objects.exists():
         checkerObj = list_objects.first()
         profile_data = CheckerTaskSerializer(checkerObj, many=False)
-        # checkerObj.status = LinkStatus.suspended
-        # checkerObj.save()
+        checkerObj.status = LinkStatus.suspended
+        checkerObj.save()
         user = checkerObj.owner
         userTelegram_objs = UserTelegram.objects.filter(user=user)
         user_telegram = userTelegram_objs.first()

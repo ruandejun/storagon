9b28cff8574cba4e735207088ca711f2cf4d76bc
ruandejun <ruandejun@gmail.com>
2023-02-15 15:33:15 +1000

    update new gpt

--- telegram_bot/task.py
+++ telegram_bot/task.py
@@ -335,7 +335,7 @@ def create_completion_openai(text):
         #engine = "code-cushman-001",
         prompt = prompt,
         temperature = 0.9,
-        max_tokens = 500,
+        max_tokens = 1500,
         top_p = 1,
         frequency_penalty = 0,
         presence_penalty = 0.6,

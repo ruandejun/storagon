c0de3be92707edd710aa31c249d6646016cb3532
ruandejun <ruandejun@gmail.com>
2023-04-22 10:56:25 +1000

    update rediect url

--- telegram_bot/task.py
+++ telegram_bot/task.py
@@ -235,10 +235,11 @@ def create_html_cashback_product_show(product_json):
     html_show = '''
 <a href="%s"> </a>
 Giá gốc: %s
+Phiếu giảm giá: %s
+Giá cuối: %s
 Chiết khấu: %s
-Phiếu khuyến mãi: %s
 %s
-''' % (product_json['pict_url'], product_json['zk_final_price'], round(product_json['commission_price'], 2), product_json['coupon_amount'], product_json['taokouling'] )
+''' % (product_json['pict_url'], product_json['zk_final_price'], product_json['coupon_amount'], product_json['zk_final_price']-product_json['coupon_amount'], round(product_json['commission_price'], 2), product_json['taokouling'] )
     return html_show
 
 def create_html_show(type='',balance='',total='',page='',total_page='',updated='', status='', plant_text='',displaying_page='Displaying'):

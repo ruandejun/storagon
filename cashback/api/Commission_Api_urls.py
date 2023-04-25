20c2c5035b360d58723e754066ff5a9ef4f65fdd
ruandejun <ruandejun@gmail.com>
2023-04-25 21:35:14 +1000

    update new api

--- cashback/api/Commission_Api_urls.py
+++ cashback/api/Commission_Api_urls.py
@@ -11,4 +11,13 @@ urlpatterns = [
 	url(r'^get_commission/', get_commission, name='get_commission'),
 	url(r'^get_link_mobile/', get_link_mobile, name='get_link_mobile'),
 	url(r'^get_link_pc/', get_link_pc, name='get_link_pc'),
+	url(r'^get_list_taobao_order_commission/', get_list_taobao_order_commission, name='get_list_taobao_order_commission'),
+	url(r'^get_list_1688_order_commission/', get_list_1688_order_commission, name='get_list_1688_order_commission'),
+	url(r'^get_1688_commission_statistics/', get_1688_commission_statistics, name='get_1688_commission_statistics'),
+	url(r'^get_taobao_commission_statistics/', get_taobao_commission_statistics, name='get_taobao_commission_statistics'),
+	url(r'^get_all_commission/', get_all_commission, name='get_all_commission'),
+	url(r'^get_item_selections/', get_item_selections, name='get_item_selections'),
+	url(r'^get_item_quantity_sale/', get_item_quantity_sale, name='get_item_quantity_sale'),
+	url(r'^get_item_flash_sale/', get_item_flash_sale, name='get_item_flash_sale'),
+	url(r'^get_item_ifashions/', get_item_ifashions, name='get_item_ifashions'),
 ]

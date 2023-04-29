4a55b5be5a7179b173cd6a8e41a7ac40f0e24056
ruandejun <ruandejun@gmail.com>
2023-04-29 13:43:38 +1000

    update status 1688

--- cashback/models/payment_models.py
+++ cashback/models/payment_models.py
@@ -557,9 +557,12 @@ class TransactionCommission(models.Model):
     transaction_holder = models.ForeignKey("BalanceAccount", verbose_name=_("transaction_holder"), blank=True,
                                            null=True,
                                            on_delete=models.PROTECT)
-
+    
+    commission_amount = models.DecimalField(verbose_name=_("commission_amount"), default=decimal.Decimal(0), max_digits=MONEY_MAX_DIGITS,
+                                 decimal_places=MONEY_DECIMAL_PLACES, validators=[MinValueValidator(0)], db_index=True)
+    
     amount = models.DecimalField(verbose_name=_("amount"), default=decimal.Decimal(0), max_digits=MONEY_MAX_DIGITS,
-                                 decimal_places=MONEY_DECIMAL_PLACES, validators=[MinValueValidator(0)], db_index=True)\
+                                 decimal_places=MONEY_DECIMAL_PLACES, validators=[MinValueValidator(0)], db_index=True)
 
     customer_ratio = models.DecimalField(verbose_name=_("customer_ratio"), default=decimal.Decimal(0), max_digits=MONEY_MAX_DIGITS,
                                  decimal_places=MONEY_DECIMAL_PLACES, validators=[MinValueValidator(0)], db_index=True)

49e50bf9dc3eac133e1e2d5d0dc12a321c0c3440
ruandejun <ruandejun@gmail.com>
2023-04-29 13:46:09 +1000

    update status 1688

--- cashback/models/payment_models.py
+++ cashback/models/payment_models.py
@@ -566,7 +566,10 @@ class TransactionCommission(models.Model):
 
     customer_ratio = models.DecimalField(verbose_name=_("customer_ratio"), default=decimal.Decimal(0), max_digits=MONEY_MAX_DIGITS,
                                  decimal_places=MONEY_DECIMAL_PLACES, validators=[MinValueValidator(0)], db_index=True)
-
+    
+    share_fee = models.DecimalField(verbose_name=_("share_fee"), default=decimal.Decimal(0), max_digits=MONEY_MAX_DIGITS,
+                                 decimal_places=MONEY_DECIMAL_PLACES, validators=[MinValueValidator(0)], db_index=True)
+    
     detail = models.CharField(verbose_name=_("detail"), blank=True,max_length=512)  # store detail automatically get from bank system
 
     reference = models.CharField(verbose_name=_("reference"), blank=True, max_length=255)  # store detail automatically get from bank system

fd0dee4af6a465938d65f2fbafd3543d2b1eea0a
ruandejun <ruandejun@gmail.com>
2023-05-03 19:45:57 +1000

    update

--- storagon/settings_base.py
+++ storagon/settings_base.py
@@ -14,10 +14,6 @@ import sys
 from pathlib import Path
 from corsheaders.defaults import default_headers
 import mongoengine
-
-import django
-django.setup()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
